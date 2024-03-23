4546542177908df9854b630442b069d5931fc377
heechan <shgmlcks1004@naver.com>
2024-03-24 03:12:21 +0900

    chore: "프로젝트 생성 및 README 파일 작성"

---
+++ src/main/java/com/project/goal_challenge/GoalChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.project.goal_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoalChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoalChallengeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/goal_challenge/GoalChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.goal_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoalChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
