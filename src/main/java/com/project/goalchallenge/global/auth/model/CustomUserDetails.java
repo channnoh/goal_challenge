510f741fdae8446e9caad1463b29fa770421d0e7
heechan <shgmlcks1004@naver.com>
2024-04-02 23:53:41 +0900

    refactor: "CustomUserDetails 필요한 field만 관리하도록 수정"

--- src/main/java/com/project/goalchallenge/global/auth/model/CustomUserDetails.java
+++ src/main/java/com/project/goalchallenge/global/auth/model/CustomUserDetails.java
@@ -1,31 +1,38 @@
 package com.project.goalchallenge.global.auth.model;
 
-import com.project.goalchallenge.domain.member.entity.Member;
+import com.project.goalchallenge.domain.member.type.MemberType;
 import java.util.Collection;
 import java.util.Collections;
-import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-@RequiredArgsConstructor
 public class CustomUserDetails implements UserDetails {
 
-  private final Member member;
+  private final MemberType memberType;
+  private final String password;
+  private final String email;
+
+
+  public CustomUserDetails(MemberType memberType, String password, String email) {
+    this.memberType = memberType;
+    this.password = password;
+    this.email = email;
+  }
 
   @Override
   public Collection<? extends GrantedAuthority> getAuthorities() {
-    return Collections.singletonList(new SimpleGrantedAuthority(member.getMemberType().toString()));
+    return Collections.singletonList(new SimpleGrantedAuthority(memberType.toString()));
   }
 
   @Override
   public String getPassword() {
-    return member.getPassword();
+    return password;
   }
 
   @Override
   public String getUsername() {
-    return member.getEmail();
+    return email;
   }
 
   @Override
--- src/main/java/com/project/goalchallenge/global/auth/service/CustomUserDetailsService.java
+++ src/main/java/com/project/goalchallenge/global/auth/service/CustomUserDetailsService.java
@@ -1,5 +1,6 @@
 package com.project.goalchallenge.global.auth.service;
 
+import com.project.goalchallenge.domain.member.entity.Member;
 import com.project.goalchallenge.domain.member.repository.MemberRepository;
 import com.project.goalchallenge.global.auth.model.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
@@ -16,7 +17,9 @@ public class CustomUserDetailsService implements UserDetailsService {
 
   @Override
   public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-    return new CustomUserDetails(memberRepository.findByEmail(email)
-        .orElseThrow(() -> new UsernameNotFoundException("Couldn't find user")));
+    Member member = memberRepository.findByEmail(email)
+        .orElseThrow(() -> new UsernameNotFoundException("Couldn't find user"));
+    return new CustomUserDetails(member.getMemberType(), member.getPassword(), member.getEmail());
+
   }
 }
