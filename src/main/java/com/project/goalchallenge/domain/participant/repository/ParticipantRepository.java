326827e8a9dbbc20b13fe8253180e17b30dfd911
heechan <shgmlcks1004@naver.com>
2024-10-27 22:14:06 +0900

    Refactor: 성능 향상을 위한 JPQL 쿼리 작성
    Count 쿼리에 join 쿼리 나가지 않도록 수정

--- src/main/java/com/project/goalchallenge/domain/participant/repository/ParticipantRepository.java
+++ src/main/java/com/project/goalchallenge/domain/participant/repository/ParticipantRepository.java
@@ -2,10 +2,19 @@ package com.project.goalchallenge.domain.participant.repository;
 
 import com.project.goalchallenge.domain.participant.entity.Participant;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ParticipantRepository extends JpaRepository<Participant, Long> {
 
   boolean existsByMemberIdAndChallengeId(Long memberId, Long challengeId);
+
+  @Query("select count(*) from Participant p where p.challenge.id = :challengeId")
+  Integer countByChallengeId(@Param("challengeId") Long challengeId);
+
+  @Query("select count(*) from Participant p where p.member.id = :memberId")
+  Integer countByMemberId(@Param("memberId") Long memberId);
+
 }
